67c63b7d2c717edded96011fd57e6aa7798ea056
Eric Van <ggclarence@yahoo.com>
2022-02-01 16:58:36 -0600

    commit tree b72e20bd561aca976f38d6f2ff4ea0bc9d879f64

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  let result
+
+  if (distance <= 400) {
+    result = `This one is on me!`;
+  } 
+  else if (distance >= 2000) {
+    result = `I will gladly take your thirty bucks.`;
+        if (distance>= 2500){
+          result= "No can do."
+     }
+  }
+  return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(city){
+return city === `NYC` ? `Ok, sounds good.` : `No go.`;
+
+}
+function switchOnCharmFromTip(tip){
+let charm
+switch (tip) {
+  case `generous`:
+      charm = `Thank you so much.`;
+  break;
+  case 'not as generous':
+      charm = `Thank you.`;
+  break;
+  default: 
+    return `Bye.`;
+  }
+  return charm
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+console.log(switchOnCharmFromTip(generous))
